2b51a8fd552c2942710887c5fbc8606c152b0d6f
Brandon <sharifdramane05@gmail.com>
2026-08-08 16:45:02 +0200

    Mise a jour globale connexion BDD MySQL Alwaysdata et URLs API

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Configuration de la connexion BDD (MySQL / Render)
+// Configuration de la connexion BDD (MySQL)
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
